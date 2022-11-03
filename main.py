633b8b1aa3b3b7115954b9e367bcb6141fc69f49
segovelo <gauchosinpingo@gmail.com>
2022-11-03 19:27:29 +0000

    Add ssh commands in python

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import getpass
 from fabric import Connection, Config
 from dotenv import load_dotenv
-import os
+import os, re
 
 load_dotenv()
 pass_phrase = os.getenv("SSH_PASSPHRASE")
-password = getpass.getpass("Enter your password")
+password = getpass.getpass("Enter your root password")
 config = Config(overrides={
     "sudo": {
         "password": password
@@ -14,10 +14,25 @@ config = Config(overrides={
                 "passphrase": pass_phrase  
     }
     })
+    
 
 connect_kwargs = {
     "passphrase": pass_phrase  
     }
 
-conn = Connection("172.17.59.186", user="segovelo", config=config)
-conn.run("ls -la")
+conn = Connection("172.17.59.186", user="<USERNAME>", config=config)
+result = conn.run("ls -la", hide=True)
+#print(result.stdout)
+conn.run("pwd")
+with conn.cd("/mnt/c/users/<USERNAME>/Desktop"):
+    conn.run("touch test_file.txt")
+    conn.run("pwd")
+#conn.sudo("apt install vim")
+#conn.run("neofetch")
+
+result = conn.run("ifconfig")
+lines = result.stdout.split("\n")
+inet_lines = [l for l in lines if "inet " in l and "127.0.0.1" not in l]
+span = re.search("inet ([0-9]+\.){3}[0-9]+", inet_lines[0]).span()
+ip_address = inet_lines[0][span[0]+5:span[1]]
+print(ip_address)
